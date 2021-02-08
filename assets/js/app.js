ec33789051fc102825bbec242ef60aae03119c06
aben-bel <abenezer.sbelay@gmail.com>
2021-02-08 07:09:02 +0300

    matching cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,386 +1,373 @@
-var level1 = [{value:'<i class="fas fa-camera">',kind:"camera"},
-     {value:'<i class="fas fa-balance-scale-left">',kind:"scale"},
-     {value:'<i class="fas fa-bed"></i>',kind:"bed"},
-     {value:'<i class="fas fa-ambulance">',kind:"ambulance"},
-     {value:'<i class="fas fa-ambulance">',kind:"ambulance"},
-     {value:'<i class="fas fa-balance-scale-left">',kind:"scale"},
-     {value:'<i class="fas fa-bed"></i>',kind:"bed"},
-     {value:'<i class="fas fa-camera">',kind:"camera"},
-     {value:'<i class="fas fa-handshake-o">',kind:"handshake"},
-     {value:'<i class="fas fa-shower">',kind:"shower"},
-     {value:'<i class="fas fa-snowflake-o">',kind:"snowflake"},
-     {value:'<i class="fas fa-bell">',kind:"bell"},
-     {value:'<i class="fas fa-handshake-o">',kind:"handshake"},
-     {value:'<i class="fas fa-shower">',kind:"shower"},
-     {value:'<i class="fas fa-snowflake-o">',kind:"snowflake"},
-     {value:'<i class="fas fa-bell">',kind:"bell"}
+var level1 = [
+  {value: '<i class="fas fa-camera">', kind: 'camera'},
+  {value: '<i class="fas fa-balance-scale-left">', kind: 'scale'},
+  {value: '<i class="fas fa-bed"></i>', kind: 'bed'},
+  {value: '<i class="fas fa-ambulance">', kind: 'ambulance'},
+  {value: '<i class="fas fa-ambulance">', kind: 'ambulance'},
+  {value: '<i class="fas fa-balance-scale-left">', kind: 'scale'},
+  {value: '<i class="fas fa-bed"></i>', kind: 'bed'},
+  {value: '<i class="fas fa-camera">', kind: 'camera'},
+  {value: '<i class="fas fa-handshake-o">', kind: 'handshake'},
+  {value: '<i class="fas fa-shower">', kind: 'shower'},
+  {value: '<i class="fas fa-snowflake-o">', kind: 'snowflake'},
+  {value: '<i class="fas fa-bell">', kind: 'bell'},
+  {value: '<i class="fas fa-handshake-o">', kind: 'handshake'},
+  {value: '<i class="fas fa-shower">', kind: 'shower'},
+  {value: '<i class="fas fa-snowflake-o">', kind: 'snowflake'},
+  {value: '<i class="fas fa-bell">', kind: 'bell'},
 ]
 
-
-const addBtn = document.querySelector("#add");
-var interval; // globalVariable
-var time; // document.getElementById("Our Class")
+const addBtn = document.querySelector('#add')
+var interval // globalVariable
+var time = document.querySelector('.time')
+var cover = document.querySelector('.cover')
 
 // addBtn.addEventListener('click',addUser);
 // genrate list as the user inputs
-var openedCards = new Array();
+var openedCards = new Array()
 
-function addUser(){
-    var txt = document.getElementById('newUser');
-    //var txtValue = txt.value;
-    if(txt.value===""){
-        txt.style.borderColor = "red";
-    return;
-    }
-    var ulNode = document.getElementById('list');
-    var liNode = document.createElement("li");
-    liNode.className = "list-group-item"
-    var txtNode = document.createTextNode(txt.value);
+function addUser() {
+  var txt = document.getElementById('newUser')
+  //var txtValue = txt.value;
+  if (txt.value === '') {
+    txt.style.borderColor = 'red'
+    return
+  }
+  var ulNode = document.getElementById('list')
+  var liNode = document.createElement('li')
+  liNode.className = 'list-group-item'
+  var txtNode = document.createTextNode(txt.value)
 
-    liNode.appendChild(txtNode);
-    ulNode.appendChild(liNode);
-    txt.value = "";
+  liNode.appendChild(txtNode)
+  ulNode.appendChild(liNode)
+  txt.value = ''
 }
-function getKind(element){
-    const kind = element.firstChild.getAttribute("kind");
-    console.log(kind);
-    return kind;
+function getKind(element) {
+  console.log(element)
+  const kind = element.getAttribute('kind')
+  console.log(kind)
+  return kind
 }
 //checks for duplicates. Accepts NodeList or normal list of Node Elements and compares kinds to find duplicates.
-function check(){
-    if(getKind(openedCards[0]) === getKind(openedCards[1])){
-        
-        match();
-    }else{
-        unmatch();
-    }
+function check() {
+  if (getKind(openedCards[0]) === getKind(openedCards[1])) {
+    match()
+  } else {
+    unmatch()
+  }
 }
 
-function disable(){
-    var unmatchedElements = document.querySelectorAll(".col");
-    unmatchedElements.forEach(function(unmatchedElement){
-        unmatchedElement.setAttribute("disabled", true);
-    });
+function disable() {
+  cover.classList.add('disable')
 }
 
-function match(){
-    openedCards.forEach((e)=>{
-        e.classList.add("solved");
-        e.classList.remove('covered');
-        e.setAttribute("disabled", true);
-    });
-    openedCards.pop();
-    openedCards.pop();
+function match() {
+  openedCards.forEach(e => {
+    e.classList.add('solved')
+    e.classList.remove('covered')
+    e.setAttribute('disabled', true)
+  })
+  openedCards.pop()
+  openedCards.pop()
 }
 
 //Add populate cards
-function addImg(inpArray){
-    const cards = document.querySelectorAll("card");
+function addImg(inpArray) {
+  const cards = document.querySelectorAll('.col')
 
-    for (var i = 0; i < inpArray.length; i++){
-        cards[i].innerHTML = inpArray[i].value;
-        cards[i].setAttribute('kind', inpArray[i].kind);
-    }
+  for (var i = 0; i < inpArray.length; i++) {
+    cards[i].innerHTML = inpArray[i].value
+    cards[i].setAttribute('kind', inpArray[i].kind)
+  }
 }
 
 //shuffle array
 function shuffle(arr) {
-    var currentIndex = array.length, temp, randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temp = array[currentIndex];
-      arr[currentIndex] = array[randomIndex];
-      arr[randomIndex] = temp;
-    }
-  
-    return arr;
+  var currentIndex = arr.length,
+    temp,
+    randomIndex
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temp = arr[currentIndex]
+    arr[currentIndex] = arr[randomIndex]
+    arr[randomIndex] = temp
+  }
+
+  return arr
 }
 
-
-
 // IndexDB
-let DB;
+let DB
 
 // Creating the database
 window.onload = () => {
-        let gameDB = indexedDB.open('GameDatabase', 1);
-          
-        gameDB.onupgradeneeded = e => {
-            let db =  e.target.result;
-            if (!db.objectStoreNames.contains("MatchingGame")) {
-                let objectStore = db.createObjectStore("MatchingGame", {keyPath: "user"});
-    
-                objectStore.createIndex('user', 'user', {unique: true});
-                objectStore.createIndex('level', 'level', {unique: false});
-                objectStore.createIndex('cummulativeTime', 'cummulativeTime', {unique: false});
-    
-                console.log("Database created!");
-            }
-            else {
-                console.log("Database already exists.")
-            }      
-        }
-    
-        gameDB.onsuccess = e => {
-            console.log("Success");
-            DB = gameDB.result;
-        }
-    
-        gameDB.onerror = e => {
-            console.log('There was an error');
-        }
-};
+  let gameDB = indexedDB.open('GameDatabase', 1)
+
+  gameDB.onupgradeneeded = e => {
+    let db = e.target.result
+    if (!db.objectStoreNames.contains('MatchingGame')) {
+      let objectStore = db.createObjectStore('MatchingGame', {keyPath: 'user'})
+
+      objectStore.createIndex('user', 'user', {unique: true})
+      objectStore.createIndex('level', 'level', {unique: false})
+      objectStore.createIndex('cummulativeTime', 'cummulativeTime', {
+        unique: false,
+      })
+
+      console.log('Database created!')
+    } else {
+      console.log('Database already exists.')
+    }
+  }
+
+  gameDB.onsuccess = e => {
+    console.log('Success')
+    DB = gameDB.result
+  }
+
+  gameDB.onerror = e => {
+    console.log('There was an error')
+  }
+}
 
 // Adding users to the database
 function addUser(name) {
-    const newUser = {
-        user: name
-    }
+  const newUser = {
+    user: name,
+  }
 
-    let transaction = DB.transaction("MatchingGame", "readwrite");
-    transaction.onerror = e => alert(`Error ${e.target.error}`);
-    let objectStore = transaction.objectStore("MatchingGame");
+  let transaction = DB.transaction('MatchingGame', 'readwrite')
+  transaction.onerror = e => alert(`Error ${e.target.error}`)
+  let objectStore = transaction.objectStore('MatchingGame')
 
-    let request = objectStore.add(newUser);
+  let request = objectStore.add(newUser)
 
-    request.oncomplete = () => {
-        console.log("New user added");
-    }
-    request.onerror = () => {
-        console.log("There was an error")
-    }
-
+  request.oncomplete = () => {
+    console.log('New user added')
+  }
+  request.onerror = () => {
+    console.log('There was an error')
+  }
 }
 
 // Removing a user from the database
 function removeUser(name) {
+  let transaction = DB.transaction('MatchingGame', 'readwrite')
+  let objectStore = transaction.objectStore('MatchingGame')
 
-    let transaction = DB.transaction('MatchingGame', 'readwrite');
-    let objectStore = transaction.objectStore('MatchingGame');
-
-    objectStore.delete(name);
-
+  objectStore.delete(name)
 }
 
 // Listing all the users
 function getUsers() {
+  let transaction = DB.transaction('MatchingGame', 'readonly')
+  let objectStore = transaction.objectStore('MatchingGame')
+  let request = objectStore.openCursor()
 
-    let transaction = DB.transaction('MatchingGame', 'readonly');
-    let objectStore = transaction.objectStore('MatchingGame');
-    let request = objectStore.openCursor();
-
-    request.onsuccess = e => {
-        let cursor = e.target.result;
-        if (cursor) {
-            if (cursor.key) {
-                return cursor.key;
-            }
-            else {
-                cursor.continue();
-            }
-        }
+  request.onsuccess = e => {
+    let cursor = e.target.result
+    if (cursor) {
+      if (cursor.key) {
+        return cursor.key
+      } else {
+        cursor.continue()
+      }
     }
+  }
 
-    request.onerror = () => {
-        console.log("There was an error");
-    }
-
+  request.onerror = () => {
+    console.log('There was an error')
+  }
 }
 
 // Showing the level a user is on
 function getLevel(user) {
+  let transaction = DB.transaction('MatchingGame', 'readonly')
+  let objectStore = transaction.objectStore('MatchingGame')
+  let request = objectStore.openCursor()
 
-    let transaction = DB.transaction('MatchingGame', 'readonly');
-    let objectStore = transaction.objectStore('MatchingGame');
-    let request = objectStore.openCursor();
-
-    request.onsuccess = e => {
-        let cursor = e.target.result;
-        if (cursor) {
-            if (cursor.key != user) {
-                cursor.continue();
-            }
-            else {
-                return cursor.value.level;
-            }
-        }
+  request.onsuccess = e => {
+    let cursor = e.target.result
+    if (cursor) {
+      if (cursor.key != user) {
+        cursor.continue()
+      } else {
+        return cursor.value.level
+      }
     }
+  }
 
-    request.onerror = () => {
-        console.log("There was an error");
-    }
-
+  request.onerror = () => {
+    console.log('There was an error')
+  }
 }
 
 // Shpwing the time it took a user to finish the game
 function getTime(user) {
+  let transaction = DB.transaction('MatchingGame', 'readonly')
+  let objectStore = transaction.objectStore('MatchingGame')
+  let request = objectStore.openCursor()
 
-    let transaction = DB.transaction('MatchingGame', 'readonly');
-    let objectStore = transaction.objectStore('MatchingGame');
-    let request = objectStore.openCursor();
-
-    request.onsuccess = e => {
-        let cursor = e.target.result;
-        if (cursor) {
-            if (cursor.key != user) {
-                cursor.continue();
-            }
-            else {
-                return cursor.value.cummulativeTime;
-            }
-        }
+  request.onsuccess = e => {
+    let cursor = e.target.result
+    if (cursor) {
+      if (cursor.key != user) {
+        cursor.continue()
+      } else {
+        return cursor.value.cummulativeTime
+      }
     }
+  }
 
-    request.onerror = () => {
-        console.log("There was an error");
-    }
-
+  request.onerror = () => {
+    console.log('There was an error')
+  }
 }
 
 function setLevel(user, level) {
-    let transaction = DB.transaction('MatchingGame', 'readwrite');
-    let objectStore = transaction.objectStore('MatchingGame');
+  let transaction = DB.transaction('MatchingGame', 'readwrite')
+  let objectStore = transaction.objectStore('MatchingGame')
 
-    let request = objectStore.openCursor();
+  let request = objectStore.openCursor()
 
-    request.onsuccess = e => {
-        let cursor = e.target.result;
-        if (cursor) {
-            if (cursor.key != user) {
-                cursor.continue();
-            }
-            else {
-                cursor.value.level = level;
-            }
-        }
+  request.onsuccess = e => {
+    let cursor = e.target.result
+    if (cursor) {
+      if (cursor.key != user) {
+        cursor.continue()
+      } else {
+        cursor.value.level = level
+      }
     }
+  }
 }
 
 // Updating the database with new levels and time
 function updateProgress(user, level, cummulativeTime) {
-    let updated = {
-        user: user,
-        level: level,
-        cummulativeTime: cummulativeTime
-    }
+  let updated = {
+    user: user,
+    level: level,
+    cummulativeTime: cummulativeTime,
+  }
 
-    let transaction = DB.transaction('MatchingGame', 'readwrite');
-    let objectStore = transaction.objectStore('MatchingGame');
+  let transaction = DB.transaction('MatchingGame', 'readwrite')
+  let objectStore = transaction.objectStore('MatchingGame')
 
-    let request = objectStore.openCursor();
+  let request = objectStore.openCursor()
 
-    request.onsuccess = e => {
-        let cursor = e.target.result;
-        if (cursor) {
-            if (cursor.key === user) {
-                var upd = objectStore.put(updated);
-                upd.onsuccess = function (e) {
-                    console.log("Update Success");
-                }
-                upd.onerror = function (e) {
-                    console.log("Update Failed");
-                }
-            }
-            cursor.continue();
+  request.onsuccess = e => {
+    let cursor = e.target.result
+    if (cursor) {
+      if (cursor.key === user) {
+        var upd = objectStore.put(updated)
+        upd.onsuccess = function (e) {
+          console.log('Update Success')
         }
+        upd.onerror = function (e) {
+          console.log('Update Failed')
+        }
+      }
+      cursor.continue()
     }
-
+  }
 }
 
-function next(currentUser){
-    if (getLevel(currentUser)!=3){
-        updateProgress(currentUser, setLevel(currentUser, getLevel(currentUser)+1), time.innerText);
-        window.open(`level${getLevel(currentUser)-1}.html`)
-    }
+function next(currentUser) {
+  if (getLevel(currentUser) != 3) {
+    updateProgress(
+      currentUser,
+      setLevel(currentUser, getLevel(currentUser) + 1),
+      time.innerText,
+    )
+    window.open(`level${getLevel(currentUser) - 1}.html`)
+  }
 }
 
 // unMatch
 function unmatch() {
-    const [cardOne, cardTwo] = openedCards;
-    cardOne.classList.add("unmatched");
-    cardTwo.classList.add("unmatched");
-    disable();
+  const [cardOne, cardTwo] = openedCards
+  cardOne.classList.add('unmatched')
+  cardTwo.classList.add('unmatched')
+  disable()
 
-    setTimeout( () => {
-        console.log(openedCards)
-        cardOne.classList.remove("unmatched", "show", "open");
-        cardTwo.classList.remove("unmatched", "show", "open");
-        cardOne.classList.add("covered");
-        cardTwo.classList.add("covered")
-        openedCards = new Array();
-        enable();
-    },1000);
-
-    
-
+  setTimeout(() => {
+    console.log(openedCards)
+    cardOne.classList.remove('unmatched', 'show', 'open')
+    cardTwo.classList.remove('unmatched', 'show', 'open')
+    cardOne.classList.add('covered')
+    cardTwo.classList.add('covered')
+    openedCards = new Array()
+    enable()
+  }, 1000)
 }
 
 // startTimer
-
 function startTimer() {
-    var sec = 0, min = 0;
-    interval = setInterval( () => {
-        let timeSec = sec;
-        let timeMin = min;
-        time.innerText = timeMin + " : " + timeSec;
-        sec++;
-        if (sec === 60) {
-            min++;
-            sec = 0;
-        }
-    }, 1000)
-}
-
-function openedCard(e){
-    openedCards.push(e.target);
-    if (openedCards.length === 2){
-        check(e.target);
+  var sec = 0,
+    min = 0
+  interval = setInterval(() => {
+    let timeSec = sec
+    let timeMin = min
+    time.textContent = timeMin + ' : ' + timeSec
+    sec++
+    if (sec === 60) {
+      min++
+      sec = 0
     }
-    
-}
-function enable(){
-    var unmatched = document.querySelectorAll('.col');
-    unmatched.forEach((e) => {
-        e.setAttribute("disabled", false);
-    });
-}
-const reload = document.querySelector('.col-3');
-reload.addEventListener('click',quit);
-function quit(){
-    location.reload;
-    window.location.href("index.html");
+  }, 1000)
 }
 
-function displayCard(e){
-    e.target.classList.add("show");
-    e.target.classList.add("open");
-    e.target.classList.remove("covered");
+function openedCard(e) {
+  if (e.target.getAttribute('disabled')) {
+    return
+  }
+  openedCards.push(e.target)
+  if (openedCards.length === 2) {
+    check(e.target)
+  }
+}
+function enable() {
+  cover.classList.remove('disable')
+}
+const reload = document.querySelector('.col-3')
+reload.addEventListener('click', quit)
+function quit() {
+  location.reload
+  window.location.href('index.html')
+}
+
+function displayCard(e) {
+  e.target.classList.add('show')
+  e.target.classList.add('open')
+  e.target.classList.remove('covered')
 }
 
 function addEventListenerToButtons() {
-    document.querySelectorAll('.col').forEach((e)=>{
-        e.addEventListener('click',openedCard);
-        e.addEventListener('click',congruatulation);
-        e.addEventListener('click',displayCard);
-    });
+  document.querySelectorAll('.col').forEach(e => {
+    e.addEventListener('click', openedCard, false)
+    e.addEventListener('click', congruatulation, false)
+    e.addEventListener('click', displayCard, false)
+  })
 }
 
-
-
-function congruatulation(){
-    if(document.querySelectorAll('.matched').length === 
-    document.querySelectorAll('[kind]').length){
-        document.querySelector('#myModal').display = 'block';
-    }
-    // TODO: end time, save score
+function congruatulation() {
+  if (
+    document.querySelectorAll('.matched').length ===
+    document.querySelectorAll('[kind]').length
+  ) {
+    interval = undefined
+    document.querySelector('#myModal').display = 'block'
+  }
+  // TODO: end time, save score
 }
 
-(function start(){
-    addImg(level1);
-    // startTimer();
-    addEventListenerToButtons();
-    console.log("start game")
-}());
+;(function start() {
+  // shuffle(level1)
+  addImg(level1)
+  startTimer()
+  addEventListenerToButtons()
+  console.log('start game')
+})()
 
 window.on
